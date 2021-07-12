09d688ac184c5636542c400c755aa98b0a377f3f
unknown <trabucco.francisco@gmail.com>
2021-07-12 18:03:35 -0300

    primer commit, variables, declaraciones, operadores y calculos matematicos básicos

---
+++ src/Calculos_conMath.java
@@ -0,0 +1,20 @@
+
+public class Calculos_conMath {
+	
+	public static void main(String args[]) {
+		
+		double raiz = Math.sqrt(9);
+		
+		System.out.println(raiz);
+		
+		double base=5;
+		
+		double exponente=3;
+		
+		int resultado= (int)Math.pow(base, exponente);
+		
+		System.out.println("El resultado de "+ base+" elevado a "+ exponente+
+				" es "+ resultado);
+		
+	}
+}
---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+	
+	public static void main(String args[]) {
+		
+		byte edad=18; //Declaración e iniciación de una variable en una línea
+		
+		/*
+		 * la siguiente instrucción imprime en consola el valor 
+		 * de la variable edad
+		 */
+		System.out.println(edad);
+		
+		edad=27;
+		
+		System.out.print(edad);
+	}
+}
