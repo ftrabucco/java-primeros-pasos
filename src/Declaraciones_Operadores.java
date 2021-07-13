a2bda777ed09eaf78bee0da6b6733e1c80a2f0ca
unknown <trabucco.francisco@gmail.com>
2021-07-12 21:38:07 -0300

    se agregan clases de manipulación de strings

--- src/Declaraciones_Operadores.java
+++ src/Declaraciones_Operadores.java
@@ -2,19 +2,19 @@
 public class Declaraciones_Operadores {
 
 	public static void main(String args[]) {
-		
-		double a=5;
-		
+
+		double a = 5;
+
 		double b;
-		
-		b=7;
-		
-		double c=b/a;
-		
+
+		b = 7;
+
+		double c = b / a;
+
 		c++;
-		
-		//c-=6;
-		
+
+		// c-=6;
+
 		System.out.print(c);
 	}
 }
--- src/Variables.java
+++ src/Variables.java
@@ -1,18 +1,17 @@
 
 public class Variables {
-	
+
 	public static void main(String args[]) {
-		
-		byte edad=18; //Declaración e iniciación de una variable en una línea
-		
+
+		byte edad = 18; // Declaración e iniciación de una variable en una línea
+
 		/*
-		 * la siguiente instrucción imprime en consola el valor 
-		 * de la variable edad
+		 * la siguiente instrucción imprime en consola el valor de la variable edad
 		 */
 		System.out.println(edad);
-		
-		edad=27;
-		
+
+		edad = 27;
+
 		System.out.print(edad);
 	}
 }
