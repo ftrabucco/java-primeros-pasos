078b4ff1a8c25df5d06e26cd33cb7eb74b38004a
unknown <trabucco.francisco@gmail.com>
2021-07-16 23:56:28 -0300

    Agrego metodos a clase Coche

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -2,15 +2,21 @@ package poo;
 
 public class Coche {
 
-	int ruedas;
+	private int ruedas;
 	
-	int largo;
+	private int largo;
 	
-	int ancho;
+	private int ancho;
 	
-	int motor;
+	private int motor;
 	
-	int peso;
+	private int peso_plataforma;
+	
+	private int peso_total;
+	
+	private String color;
+	
+	private boolean asientos_cuero, climatizador;
 	
 	public Coche() {
 		
@@ -22,8 +28,48 @@ public class Coche {
 		
 		motor=1600;
 		
-		peso=500;
+		peso_plataforma=500;
 		
 		}
+	
+	public String dime_datos_generales() { // GETTER
+		
+		return "La plataforma del vehículo tiene " + ruedas + " ruedas "+
+	". Mide " + largo/1000 + " metros con un ancho de " + ancho +
+	" cm y un peso de " + peso_plataforma + " kg";
+	}
+	
+	
+	public void establece_color(String color_coche) { 
+		
+		color = color_coche;
+	}
+	
+	public void configura_asientos(String asientos_cuero) { // SETTER
+		
+		if(asientos_cuero=="si") {
+			
+			this.asientos_cuero=true;
+			
+		} else {
+			
+			this.asientos_cuero=false;
+		}
+	}
+	
+	public String dime_asientos() { // GETTER
+		
+		if(asientos_cuero=true) {
+			
+			return "El coche tiene asientos de cuero";
+			
+		} else {
+			
+			return "El coche tiene asientos de serie";
+		}
+	}
+	
+	
+	
 
 }
