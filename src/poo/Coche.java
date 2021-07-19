7fef92632b95606f98e945ef8563a783e428b06c
unknown <trabucco.francisco@gmail.com>
2021-07-18 21:15:53 -0300

    Agrego clase uso coche y metodos a la clase coche

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -40,14 +40,19 @@ public class Coche {
 	}
 	
 	
-	public void establece_color(String color_coche) { 
+	public void establece_color(String color_coche) { // SETTER
 		
 		color = color_coche;
 	}
 	
+	public String dime_color() {
+		
+		return "El color del coche es "+color;
+	}
+	
 	public void configura_asientos(String asientos_cuero) { // SETTER
 		
-		if(asientos_cuero=="si") {
+		if(asientos_cuero.equalsIgnoreCase("si")) {
 			
 			this.asientos_cuero=true;
 			
@@ -59,7 +64,7 @@ public class Coche {
 	
 	public String dime_asientos() { // GETTER
 		
-		if(asientos_cuero=true) {
+		if(asientos_cuero==true) {
 			
 			return "El coche tiene asientos de cuero";
 			
@@ -68,6 +73,66 @@ public class Coche {
 			return "El coche tiene asientos de serie";
 		}
 	}
+	
+	public void configura_climatizador(String climatizador) { // SETTER
+		
+		if(climatizador.equalsIgnoreCase("si")) {
+			
+			this.climatizador=true;
+			
+		} else {
+			
+			this.asientos_cuero=false;
+		}
+	}
+	
+	public String dime_climatizador() { // GETTER
+		
+		if(climatizador==true) {
+			
+			return "El coche incorpora climatizador";
+		} else {
+			return "El coche lleva aire acondicionado";
+		}
+		
+	}
+	
+	public String dime_peso_coche() { 
+		
+		int peso_carroceria=500;
+		
+		peso_total=peso_plataforma+peso_carroceria;
+		
+		if(asientos_cuero) {
+			
+			peso_total=peso_total+50;
+		}
+		if(climatizador==true) {
+			
+			peso_total=peso_total+20;
+		}
+		
+		return "El peso del coche es "+peso_total;
+	}
+	
+	public int precio_coche() {
+		
+		int precio_final=10000;
+		
+		if(asientos_cuero==true) {
+			
+			precio_final+=2000;
+		}
+		
+		if (climatizador==true) {
+			
+			precio_final+=1500;
+		}
+		
+		return precio_final;
+	}
+	
+	
 	
 	
 	
