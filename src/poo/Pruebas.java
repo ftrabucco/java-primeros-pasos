65f2db1d845deb7243f7897c2af717d7f2e84a54
unknown <trabucco.francisco@gmail.com>
2021-08-15 11:17:51 -0300

    se agega método static

--- src/poo/Pruebas.java
+++ src/poo/Pruebas.java
@@ -7,9 +7,9 @@ public class Pruebas {
 		Empleados trabajador1 = new Empleados("Paco");
 
 		Empleados trabajador2 = new Empleados("Ana");
-		
+
 		Empleados trabajador3 = new Empleados("Carlos");
-		
+
 		Empleados trabajador4 = new Empleados("Franco");
 
 		trabajador1.cambiaSeccion("RRHH");
@@ -19,10 +19,12 @@ public class Pruebas {
 		System.out.println(trabajador1.devuelveDatos());
 
 		System.out.println(trabajador2.devuelveDatos());
-		
+
 		System.out.println(trabajador3.devuelveDatos());
-		
+
 		System.out.println(trabajador4.devuelveDatos());
+		
+		System.out.println(Empleados.dameIdSiguiente());
 
 	}
 }
@@ -32,26 +34,34 @@ class Empleados {
 	private String nombre;
 	private String seccion;
 	private int id;
-	private static int idSiguiente=1;
+	private static int idSiguiente = 1;
 
 	public Empleados(String nom) {
+
 		nombre = nom;
 		seccion = "Administracion";
-		id=idSiguiente;
+		id = idSiguiente;
 		idSiguiente++;
 	}
 
 	public String devuelveDatos() {
-		return "El nombre es: " + nombre + " y la sección es " + seccion +
-				" y el id es " + id;
+
+		return "El nombre es: " + nombre + " y la sección es " + seccion + " y el id es " + id;
 	}
 
 	public void cambiaSeccion(String seccion) {
+
 		this.seccion = seccion;
 	}
 
 	public void cambiaNombre(String nombre) {
+
 		this.nombre = nombre;
+	}
+
+	public static String dameIdSiguiente() {
+
+		return "El idSiguiente es:" + idSiguiente;
 	}
 
 }
