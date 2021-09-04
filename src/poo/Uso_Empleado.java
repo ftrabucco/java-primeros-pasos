32386e9585146d8a775e6080830711413c5e5b2b
unknown <trabucco.francisco@gmail.com>
2021-09-04 14:42:29 -0300

    Agrego clase PruebaTemporizador y DameLaHora; implemento interfaz ActionListener

--- src/poo/Uso_Empleado.java
+++ src/poo/Uso_Empleado.java
@@ -32,7 +32,6 @@ public class Uso_Empleado {
 		 * dameSueldo()+ " Fecja de Alta: "+ empleado3.dameFechaContrato());
 		 */
 
-		
 		/*
 		 * for(int i=0;i<3;i++) {
 		 * 
@@ -48,10 +47,9 @@ public class Uso_Empleado {
 		 * 
 		 * }
 		 */
-		
-		
-		Jefatura jefe_RRHH=new Jefatura("Fran",50000,2000,07,07);
-		
+
+		Jefatura jefe_RRHH = new Jefatura("Fran", 50000, 2000, 07, 07);
+
 		jefe_RRHH.estableceIncentivo(9000);
 
 		Empleado[] misEmpleados = new Empleado[5];
@@ -61,44 +59,43 @@ public class Uso_Empleado {
 		misEmpleados[1] = new Empleado("Ana López", 95000, 1995, 6, 02);
 
 		misEmpleados[2] = new Empleado("Paco Gómez", 105000, 2002, 03, 15);
-		
-		misEmpleados[3] = jefe_RRHH; //Polimorfismo en acción. Principio de sustitución
-		
-		misEmpleados[4]=new Jefatura("María",95000,1999,5,26);
-		
-		Jefatura jefe_Finanzas=(Jefatura)misEmpleados[4];
-		
+
+		misEmpleados[3] = jefe_RRHH; // Polimorfismo en acción. Principio de sustitución
+
+		misEmpleados[4] = new Jefatura("María", 95000, 1999, 5, 26);
+
+		Jefatura jefe_Finanzas = (Jefatura) misEmpleados[4];
+
 		jefe_Finanzas.estableceIncentivo(10500);
-		
-		System.out.println(jefe_Finanzas.tomar_decision("Dar más días de vacaciones a todos"
-				+ "los empleados"));
-		
+
+		System.out.println(jefe_Finanzas.tomar_decision("Dar más días de vacaciones a todos" + "los empleados"));
+
 		jefe_Finanzas.establece_bonus(5000);
-		
-		System.out.println("El jefe "+jefe_Finanzas.dameNombre()+" tiene un bonus de: "+
-		jefe_Finanzas.establece_bonus(500));
-		
-		System.out.println(misEmpleados[3].dameNombre()+" tiene un bonus de: "+
-		misEmpleados[3].establece_bonus(200));
-		
+
+		System.out.println(
+				"El jefe " + jefe_Finanzas.dameNombre() + " tiene un bonus de: " + jefe_Finanzas.establece_bonus(500));
+
+		System.out
+				.println(misEmpleados[3].dameNombre() + " tiene un bonus de: " + misEmpleados[3].establece_bonus(200));
+
 		for (Empleado e : misEmpleados) {
 
 			e.subeSueldo(5);
 		}
-		
+
 		Arrays.sort(misEmpleados);
 
 		for (Empleado e : misEmpleados) {
 
-			System.out.println(e.dameNombre() + " Sueldo: " + e.dameSueldo() + " Fecha de Alta: "
-					+ e.dameFechaContrato());
+			System.out.println(
+					e.dameNombre() + " Sueldo: " + e.dameSueldo() + " Fecha de Alta: " + e.dameFechaContrato());
 		}
 
 	}
 
 }
 
-class Empleado implements Comparable, Trabajadores{
+class Empleado implements Comparable, Trabajadores {
 
 	public Empleado(String nom, double sue, int agno, int mes, int dia) {
 
@@ -107,25 +104,24 @@ class Empleado implements Comparable, Trabajadores{
 		GregorianCalendar calendario = new GregorianCalendar(agno, mes - 1, dia);
 		altaContrato = calendario.getTime();
 		++idSiguiente;
-		id=idSiguiente;
+		id = idSiguiente;
 	}
 
 	public Empleado(String nom) {
 
-		this(nom,30000,2000,01,01);
+		this(nom, 30000, 2000, 01, 01);
 	}
 
-	
 	private String nombre;
 	private double sueldo;
 	private Date altaContrato;
 	private String seccion;
-	private int id=1;
+	private int id = 1;
 	private static int idSiguiente;
-	
+
 	public String dameNombre() {
 
-		return "ID: "+ id +" Nombre: "+nombre;
+		return "ID: " + id + " Nombre: " + nombre;
 	}
 
 	public double dameSueldo() {
@@ -148,44 +144,44 @@ class Empleado implements Comparable, Trabajadores{
 	@Override
 	public int compareTo(Object miObjeto) {
 		// TODO Auto-generated method stub
-		Empleado otroEmpleado=(Empleado)miObjeto;
-		
-		if(this.id<otroEmpleado.id) {
+		Empleado otroEmpleado = (Empleado) miObjeto;
+
+		if (this.id < otroEmpleado.id) {
 			return -1;
 		}
-		if(this.id>otroEmpleado.id) {
+		if (this.id > otroEmpleado.id) {
 			return 1;
 		}
 		return 0;
 	}
-	
+
 	public double establece_bonus(double gratificacion) {
-		
-		return Trabajadores.bonus_base+gratificacion;
-		
+
+		return Trabajadores.bonus_base + gratificacion;
+
 	}
 }
 
-class Jefatura extends Empleado implements Jefe{
+class Jefatura extends Empleado implements Jefe {
 
 	public Jefatura(String nom, double sue, int agno, int mes, int dia) {
-		
+
 		super(nom, sue, agno, mes, dia);
-		
+
 	}
-	
+
 	private double incentivo;
-	
+
 	public void estableceIncentivo(double b) {
-		
-		incentivo=b;
+
+		incentivo = b;
 	}
-	
+
 	public double dameSueldo() {
-		
-		double sueldoJefe=super.dameSueldo();
-		
-		return sueldoJefe+incentivo;
+
+		double sueldoJefe = super.dameSueldo();
+
+		return sueldoJefe + incentivo;
 	}
 
 	@Override
@@ -193,16 +189,12 @@ class Jefatura extends Empleado implements Jefe{
 		// TODO Auto-generated method stub
 		return "Un miembro de la dirección ha tomado la decisión de:" + decision;
 	}
-	
+
 	public double establece_bonus(double gratificacion) {
-		
-		double prima=2000;
-		
-		return Trabajadores.bonus_base+gratificacion+prima;
+
+		double prima = 2000;
+
+		return Trabajadores.bonus_base + gratificacion + prima;
 	}
-	
-	
+
 }
-
-
-
