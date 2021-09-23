4faa82d9798632316cff08326ddcd44bfd283cf7
unknown <trabucco.francisco@gmail.com>
2021-09-22 21:19:05 -0300

    agrego dibujos de figuras geometricas en Frames

--- src/graficos/PruebaDibujo.java
+++ src/graficos/PruebaDibujo.java
@@ -2,6 +2,7 @@ package graficos;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.geom.*;
 
 public class PruebaDibujo {
 
@@ -38,10 +39,35 @@ class LaminaConFiguras extends JPanel {
 
 		super.paintComponent(g);
 
-		//g.drawRect(50, 50, 200, 200);
-		
-		//g.drawLine(100, 100, 300, 200);
-		
-		g.drawArc(50, 100, 100, 200, 120, 150);
+		Graphics2D g2 = (Graphics2D) g;
+
+		Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200.25, 150);
+
+		g2.draw(rectangulo);
+
+		Ellipse2D elipse = new Ellipse2D.Double();
+
+		elipse.setFrame(rectangulo);
+
+		g2.draw(elipse);
+
+		g2.draw(new Line2D.Double(100, 100, 300, 250));
+
+		double CentroenX = rectangulo.getCenterX();
+
+		double CentroenY = rectangulo.getCenterY();
+
+		double radio = 150;
+
+		Ellipse2D circulo = new Ellipse2D.Double();
+
+		circulo.setFrameFromCenter(CentroenX, CentroenY, CentroenX + radio, CentroenY + radio);
+
+		g2.draw(circulo);
+
+		/*
+		 * g.drawRect(50, 50, 200, 200); g.drawLine(100, 100, 300, 200); g.drawArc(50,
+		 * 100, 100, 200, 120, 150);
+		 */
 	}
 }
